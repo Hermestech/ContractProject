c1195de3dcecff5ef45b7afa9ea2e32845919e47
Hermes Aguilar <adanhermes23@gmail.com>
2022-01-12 06:28:29 -0600

    adding server script

--- server/main.py
+++ server/main.py
@@ -1,10 +1,14 @@
 # Python
+import asyncio
 from typing import Optional
 # Pydantic
 from pydantic import BaseModel
 # FastAPI
 from fastapi import FastAPI
 from fastapi import Body
+import uvicorn
+
+
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
@@ -50,4 +54,25 @@ def home():
 
 @app.post('/contract/new')
 def create_contract(requirements: ContractRequirementsPF = Body(...)):
-    return requirements
+    print(requirements)
+
+
+def run_server():
+    loop = asyncio.get_event_loop()
+    server_configuration = uvicorn.Config(
+        app="main:app",
+        host='127.0.0.1',
+        port=8000,
+        loop=loop,
+        reload=True
+    )
+    server = uvicorn.Server(server_configuration)
+    loop.run_until_complete(server.serve())
+
+
+def main():
+    run_server()
+
+
+if __name__ == '__main__':
+    main()
